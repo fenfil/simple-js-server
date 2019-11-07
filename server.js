e034a6322b54d16c461201412f6c6d8e39605566
fenfil <vladimir.zaykovskiy@ya.ru>
2019-11-07 09:40:21 +0300

    a

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 const port = 3000;
 
 app
-  .use(express.static('public'))
-  .use(helmet())
-  .use(morgan('dev'))
-  .use(compression())
+.use(helmet())
+.use(morgan('dev'))
+.use(compression())
+.use(express.static('public'))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
